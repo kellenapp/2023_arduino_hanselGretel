bd8704c178e44adfcaa95773f57d9ac0f582b49f
kellenapp <122336686+kellenapp@users.noreply.github.com>
2023-10-19 10:18:16 -0400

    Come back

--- Music Code/MusicArduinoCode/MusicArduinoCode.ino
+++ Music Code/MusicArduinoCode/MusicArduinoCode.ino
@@ -1,6 +1,35 @@
 const int buttonPin = 2;
 byte arbitraryCode = 97;
 int buttonState = 0;
+#include <Servo.h>
+//servo library 
+
+//flex sensor connection
+#define fsrpin A0
+
+//first servo for kids
+Servo hanselGretel;
+
+//second servo for the cake
+Servo cake;
+
+//oven lights
+int redPin = 4;
+
+//reset button on breadboard for us
+int resetButton = 2;
+
+//start button needed
+
+//servo position
+int pos = 0;
+
+//boolean to check whether the oven door has been closed
+bool doorClosed = false;
+
+//integer to take in the pressure noted on the sensor
+int fsrreading;
+
 void setup() {
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
